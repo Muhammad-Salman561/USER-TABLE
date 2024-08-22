dfe9bb89ddeb50290977a813bd0412d366016588
Muhammad-Salman561 <SalmanOdhejo@gmail.com>
2024-08-23 01:29:11 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let userTable = prompt("Wich table to yo need?")
+let count = 1
+document.write("<b>Table of</b> " + userTable + "<br />");
+
+document.write(userTable +  " X "  + count +  " = "  + userTable*count + "<br />") ;
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br/>");
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br />");
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br />");
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br />");
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br />");
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br />");
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br />");
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br />");
+document.write(userTable +  " X "  + ++count +  " = "  + userTable*count + "<br />");
+
+
+
+
+
+
+
+
+
